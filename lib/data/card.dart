036e53160ac2b4c93652f9e57362964dea4718bb
didi ferdillah <43023277+didiferdillah23@users.noreply.github.com>
2021-09-15 10:49:20 +0700

    Add files via upload

---
+++ lib/data/card.dart
@@ -0,0 +1,18 @@
+var cards = [
+  {
+    "id": "1",
+    "card_brand": "DFPay",
+    "card_img": "assets/img/df_card.png",
+    "balance": "Rp 17.012.000",
+    "no": "1723 **** **** ****",
+    "name": "Lorem Ipsum, Jr."
+  },
+  {
+    "id": "2",
+    "card_brand": "TDFPay",
+    "card_img": "assets/img/tdf_card.png",
+    "balance": "Rp 2.012.000",
+    "no": "1093 **** **** ****",
+    "name": "Lorem Ipsum, Jr."
+  }
+];
---
+++ lib/data/transactions.dart
@@ -0,0 +1,34 @@
+var transactions = [
+  {
+    "id": "1",
+    "name": "YouTube Premium",
+    "logo": "assets/img/youtube.png",
+    "date": "17 Jan 2021",
+    "price": "- 59K",
+    "cash_in": false
+  },
+  {
+    "id": "2",
+    "name": "PayPal Payment",
+    "logo": "assets/img/paypal.png",
+    "date": "15 Jan 2021",
+    "price": "+ 7.5Jt",
+    "cash_in": true
+  },
+  {
+    "id": "3",
+    "name": "Spotify Monthly",
+    "logo": "assets/img/spotify.png",
+    "date": "2 Jan 2021",
+    "price": "- 109K",
+    "cash_in": false
+  },
+  {
+    "id": "4",
+    "name": "Starbucks Coffee",
+    "logo": "assets/img/starbucks.png",
+    "date": "1 Jan 2021",
+    "price": "- 49K",
+    "cash_in": false
+  },
+];
